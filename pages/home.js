6755d15fc53102c125d52735c681704903052e11
Senam-Kudjo <senamhumado56@gmail.com>
2023-02-18 04:05:08 +0000

    now on the homepage ... figuring out some things

--- pages/home.js
+++ pages/home.js
@@ -2,40 +2,71 @@ import { MaterialIcons } from "@expo/vector-icons";
 import React from "react";
 import {
   SafeAreaView,
+  ScrollView,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
-  KeyboardEventName,
-  ScrollView,
 } from "react-native";
+import { Ionicons, Entypo } from "@expo/vector-icons";
+import { ImageBackground } from "react-native";
 
 export default function HomeScreen({ navigation }) {
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "white",
-        display: "flex",
-        // height: "100%",
-      }}
-    >
-      <ScrollView>
-        <View style={styles.mainScreen}>
-          <View>
-            <TouchableOpacity>
-              <TextInput placeholder="Search" />
-            </TouchableOpacity>
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    // <SafeAreaView style={styles.mainScreen}>
+    <View style={styles.homeScreen}>
+      <View style={styles.firstLane}>
+        <Entypo name="bell" size={24} color="black" />
+      </View>
+      <View>
+        <TouchableOpacity style={styles.searchBar}>
+          <TextInput placeholder="Search ..." style={styles.searchBarText} />
+          <Ionicons
+            name="ios-search-outline"
+            size={24}
+            color="grey"
+            style={{ paddingRight: 10 }}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={{ height: 530 }}></View>
+    </View>
+    // </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  mainScreen: {},
+  homeScreen: {
+    height: "100%",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    // backgroundColor: "#040009",
+    backgroundColor: "white",
+    position: "absolute",
+  },
+  searchBar: {
+    backgroundColor: "#F0F0F0",
+    width: "90%",
+    height: 40,
+    justifyContent: "center",
+    borderRadius: 7,
+    shadowRadius: 5,
+    shadowColor: "silver",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  searchBarText: {
+    paddingLeft: 10,
+    width: "90%",
+    paddingTop: 0,
+  },
+  firstLane: {
+    marginBottom: 27,
+    justifyContent: "flex-end",
+    alignSelf: "flex-end",
+    paddingRight: 20,
+  },
 });
